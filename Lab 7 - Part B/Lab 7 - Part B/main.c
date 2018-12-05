59ba82e3efefe95d64ee743b60a523c41c3c2990
William Wright <william86370@gmail.com>
2018-12-05 11:20:12 -0800

    Created Lab-7 Part B

---
+++ Lab 7 - Part B/Lab 7 - Part B/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab 7 - Part B
+//
+//  Created by Anonymous on 12/5/18.
+//  Copyright © 2018 SilverTap. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
